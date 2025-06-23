06612e73fcb5fcf430d6cf6ec587e62d5cb03cfa
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-06-23 15:14:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,13 +64,8 @@ class Helios:
 
     def welcome(self):
         print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "\n" + "═" * 60)
-        print(Fore.GREEN + Style.BRIGHT + "    ⚡ Auto Helios Testnet Automation BOT ⚡")
-        print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
-        print(Fore.YELLOW + Style.BRIGHT + "    🧠 Project    : Helios - Testnet Automation")
-        print(Fore.YELLOW + Style.BRIGHT + "    🧑‍💻 Author     : YetiDAO")
-        print(Fore.YELLOW + Style.BRIGHT + "    🌐 Status     : Running | Monitoring Tasks...")
-        print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
-        print(Fore.MAGENTA + Style.BRIGHT + "    🧬 Powered by Cryptodai3 × YetiDAO | Buddy v1.2 🚀")
+        print(Fore.GREEN + Style.BRIGHT + "   Auto Helios Bot")
+        print(Fore.MAGENTA + Style.BRIGHT + "  Powered by Zonaairdrop")
         print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "═" * 60 + "\n")
 
     def format_seconds(self, seconds):
